687bddeace617a3424515fc8d88db80aadb13760
spamserver <45236565+spamserver@users.noreply.github.com>
2018-11-21 20:49:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,21 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+console.log("Scrpit By Dream");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
 
-client.on('ready', async() => {
-var server = "510562350177320960"; // ايدي السررفر
-var channel = "510562519056646156";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
+client.on("ready", () => {
+let channel =     client.channels.get("514729634030551051")
+setInterval(function() {
+channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam `);
+}, 25)
 })
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
  
-client.login(NTA4MDI1MjU2ODcxNDYwODc0.Ds7Ptg.UtUY60UF937mieeNwb0c5KBJP1o);
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == "5say") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login('NTE0ODcwMzUxNTEyMjA3Mzkw.Dtc41g.UEFHs2cQjmxVqpfrpxYqa9GlsYE');
